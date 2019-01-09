814d78b0329c613cdb5d09d047784ae9b1daea29
MATA <jinho_46@naver.com>
2019-01-09 18:47:28 +0900

    body-parser 모듈을 이용한 POST방식 데이터 전달

--- app.js
+++ app.js
@@ -6,6 +6,9 @@
 var express = require('express'); // express 모듈 불러오다.
 var app = express();
 
+// body-parser
+var bodyParser = require('body-parser');
+
 // 템플릿 엔진 사용
 app.set('view engine', 'jade');
 // 생략 가능(생략하게 되면 디펄트로 views 디렉토리를 잡는다.)
@@ -13,12 +16,50 @@ app.set('views', './views');
 
 app.use(express.static('public'));
 
+app.use(bodyParser.urlencoded({extended: false}));
+
 // html 코드를 이쁘게 해준다.
 app.locals.pretty = true;
 
-app.get('/topic', function(req, res){
-  res.send(req.query.id);
+app.get('/form', function(req, res){
+  res.render('form')
 });
+
+app.get('/form_receiver', function(req, res){
+  var title = req.query.title;
+  var description = req.query.description;
+
+  res.send(title + ', ' + description);
+});
+
+app.post('/form_receiver', function(req, res){
+  var title = req.body.title;
+  var description = req.body.description;
+
+  res.send(title + ', ' + description);
+});
+
+app.get('/topic/:id/:mode', function(req, res){
+  res.send(req.params.id + ', ' + req.params.mode);
+});
+
+// 쿼리스트링 활용
+app.get('/topic/:id', function(req, res){
+  var topics = [
+    'Javascript is...',
+    'Nodejs is...',
+    'Express is...'
+  ];
+
+  var output = `
+  <a href="/topic/0">JavaScript</a><br>
+  <a href="/topic/1">Nodejs</a><br>
+  <a href="/topic/2">Express</a><br><br>
+  ${topics[req.params.id]}
+  `
+  res.send(output);
+});
+
 
 // template으로 요청이 들어오면 render를 이용해 views 디렉토리의 temp를 실행시킨다.
 // 두번째 인자는 변수를 전달해준다.
